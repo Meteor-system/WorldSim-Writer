0fd99715ca2a73cd3355a398ed5d953777280a9f
Meteor-system <hanbaohuacc@163.com>
2026-07-10 04:00:26 +0800

    test-relation-contract

--- backend/tests/test_relation_crud.py
+++ backend/tests/test_relation_crud.py
@@ -240,6 +240,7 @@ def test_relation_create_rejects_extra_fields_without_side_effects(client, db_se
     )
 
     assert response.status_code == 422
+    assert any(error['type'] == 'extra_forbidden' and error['loc'][-1] == 'raw_text' for error in response.json()['detail'])
     after_relations = client.get(f'/worlds/{world_id}/relations', headers=auth(token)).json()
     assert after_relations == before_relations
     assert world_state(db_session, world_id).world_version == before_world_version
@@ -261,6 +262,7 @@ def test_relation_update_rejects_extra_fields_without_side_effects(client, db_se
     )
 
     assert response.status_code == 422
+    assert any(error['type'] == 'extra_forbidden' and error['loc'][-1] == 'raw_text' for error in response.json()['detail'])
     after_relation = client.get(f'/relations/{existing_id}', headers=auth(token)).json()
     assert after_relation == before_relation
     assert world_state(db_session, world_id).world_version == before_world_version
